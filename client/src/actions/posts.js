67dcb65be9c262b1ec058aa88b0eed87cd203f9a
unknown <maharajdhara95@gmail.com>
2022-03-28 13:03:55 +0530

    first

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -16,7 +16,7 @@ export const createPost = (post) => async (dispatch) => {
   try {
     const { data } = await api.createPost(post);
 
-    dispatch({ type: CREATE, payload: data });
+    dispatch({ type: CREATE, payload: data } );console.log(data)
   } catch (error) {
     console.log(error.message);
   }
